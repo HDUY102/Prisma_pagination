d5c172068f33ee06d6da6df6dbee6a0e12221f76
HDUY <leduy020702@gmail.com>
2024-07-26 10:07:06 +0700

    Update pagination

--- app/components/pagination/Pagination.tsx
+++ app/components/pagination/Pagination.tsx
@@ -9,22 +9,22 @@ import {
     PaginationNext,
     PaginationPrevious,
 } from "@/components/ui/pagination"
+import { useSearchParams } from 'next/navigation'
 
-const PaginationPage = () => {
+const PaginationPage = ({onPageChange}:any) => {
     const [totalPages, setTotalPages] = useState(0)
-    const [currentPage, setCurrentPage] = useState(1)
+    const searchParams = useSearchParams()
+    const currentPage = Number(searchParams.get('currentPage'))
+
     useEffect(()=>{
         const fetchPagination = async () =>{
             try {
-                const response = await fetch("/api/phantrang", {
+                const response = await fetch(`/api/phantrang?currentPage=${currentPage}`, {
                     method: "GET"
                 });
                 if(response.ok){
                     const data = await response.json();
                     setTotalPages(data.meta.totalPages);
-                    setCurrentPage(data.meta.currentPage);
-                    console.log("total ",totalPages)
-                    console.log("currentPage ",currentPage)
                 }
             } catch (error) {
                 console.error("Failed to fetch pagination data", error);
@@ -32,36 +32,45 @@ const PaginationPage = () => {
         }
         fetchPagination();
     },[totalPages,currentPage])
+
     const handleChangePage = (page:number) => {
-        if (page >0 && page<currentPage){
-            setCurrentPage(page)
+        if (page > 0 && page <= totalPages){
+            onPageChange(page)
         }
     }
-  return (
-    <div>
-        <Pagination>
-            <PaginationContent>
-                <PaginationItem>
-                    <PaginationPrevious href="#" onClick={()=>{
-                        handleChangePage(currentPage-1)
-                    }} disabled={currentPage === 1}/>
-                </PaginationItem>
-                <PaginationItem>
-                    <PaginationLink href="#">1</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                    <PaginationEllipsis />
-                </PaginationItem>
-                <PaginationItem>
-                    <PaginationNext href="#" onClick={()=>{
-                        handleChangePage(currentPage+1)
-                    }} disabled={currentPage === totalPages}/>
-                </PaginationItem>
-            </PaginationContent>
-        </Pagination>
 
-    </div>
-  )
+    return (
+        <div>
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious onClick={()=>{
+                            handleChangePage(currentPage-1)
+                        }} disabled={currentPage === 1}/>
+                    </PaginationItem>
+                    {Array.from({length: totalPages},(_,index) =>(
+                        <PaginationItem>
+                            <PaginationLink 
+                                isActive={ index+1 === currentPage}
+                                onClick={()=> handleChangePage(index+1)}
+                            >
+                                {index+1}
+                            </PaginationLink>
+                        </PaginationItem>
+                    ))}
+                    <PaginationItem>
+                        {currentPage >= totalPages-2 ? '' : <PaginationEllipsis />}
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext onClick={()=>{
+                            handleChangePage(currentPage+1)
+                        }} disabled={currentPage === totalPages}/>
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
+
+        </div>
+    )
 }
 
 export default PaginationPage
