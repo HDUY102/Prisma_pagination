6152ef5fc668211da316625a0631031deff210cf
HDUY <leduy020702@gmail.com>
2024-07-29 23:34:33 +0700

    Update code Slider.tsx

--- app/components/Slider.tsx
+++ app/components/Slider.tsx
@@ -33,41 +33,6 @@ const Slider = () => {
            </div>
         ))}
        </Zoom>
-      {/* <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img src="img1.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img src="img2.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img src="img3.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide4" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
