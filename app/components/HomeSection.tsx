59b3f270f871404f4f5d2734533d24cd5c5a2963
HDUY <leduy020702@gmail.com>
2024-07-31 22:56:35 +0700

    Update HomeSection.tsx

--- app/components/HomeSection.tsx
+++ app/components/HomeSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import style from "@/app/style/HomeSection.module.css";
+import style from "@/app/components/style/HomeSection.module.css";
 
 const HomeSection = () => {
   return (
