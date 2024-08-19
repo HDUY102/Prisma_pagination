bec90f195dda050a072f2554a3aa69bd9eccf28f
HDUY <leduy020702@gmail.com>
2024-08-19 20:45:38 +0700

    Update code in login folder

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,28 +11,51 @@ import {
   FormField,
   FormItem,
   FormLabel,
-  FormDescription
+  FormDescription,
+  FormMessage
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { PasswordInput } from "@/components/ui/password_input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { FiLock } from "react-icons/fi";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
-  password: z.string().min(2).max(50),
+  email: z.string().min(3,{ message: "email ít nhất 3 ký tự"}).max(50,{
+    message: "email nhiều nhất 50 ký tự"
+  }),
+  password: z.string().min(3,{message: "password ít nhất 3 ký tự"}).max(50,{
+    message: "password nhiều nhất 50 ký tự"
+  })
 });
 
 const LoginPage = () => {
+  const router = useRouter()
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      email: "",
       password: ""
     },
   });
-  const handleSubmit = () => {};
+  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+    const {email,password} = data
+    const response = await fetch("/api/login",{
+      method: "POST",
+      body: JSON.stringify({email,password})
+    })
+
+    if(response.ok){
+      const data = await response.json()
+      if(data.success){
+        localStorage.setItem("token",data.success)
+      }
+      if(data.role == 1){
+        router.push("/admin")
+      }else router.push("/")
+    }
+  };
   return (
     <div className={style.loginContainer}>
       <div className={style.welcomeLogin}>
@@ -47,21 +70,22 @@ const LoginPage = () => {
       </div>
       <div className={style.formLogin}>
       <Form {...form}>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={form.handleSubmit(handleSubmit)}>
             <FormDescription className="text-6xl text-center font-serif mb-8">
               Đăng nhập
             </FormDescription>
             <FormField
               control={form.control}
-              name="username"
+              name="email"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel className="text-4xl font-serif">
-                    Username
+                    Email
                   </FormLabel>
                   <FormControl>
-                    <Input placeholder="Username" {...field}/>
+                    <Input placeholder="email" {...field}/>
                   </FormControl>
+                  <FormMessage className="text-red-500 text-xl" />
                 </FormItem>
               )}
             />
@@ -76,9 +100,10 @@ const LoginPage = () => {
                   <FormControl>
                     <PasswordInput
                       {...field}
-                      placeholder="Password"
+                      placeholder="password"
                     />
                   </FormControl>
+                  <FormMessage className="text-red-500 text-xl" />
                 </FormItem>
               )}
             />
@@ -95,6 +120,12 @@ const LoginPage = () => {
         <Link href="/" className={style.closeLogin}>
           <button>X</button>
         </Link>
+        <p className={style.changePage}>
+          Bạn chưa có tài khoản?{" "}
+          <a className="hover:text-blue-600 hover:underline" href={"/register"}>
+            <u>Đăng Ký</u>
+          </a>
+        </p>
       </div>
     </div>
   );
